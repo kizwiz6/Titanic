aeb9a16eb14e0b5943d75db51565b4b1c79f88e1
Kieran Emery <kizwiz@hotmail.co.uk>
2023-02-23 18:32:35 +0000

    Add project files.

---
+++ FilePassengerDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titanic
+{
+    public class FilePassengerDataProvider : IPassengerDataProvider
+    {
+        private readonly string _fileName;
+
+        public FilePassengerDataProvider(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public Passenger[] GetAllPassengers()
+        {
+            // Read passengers from file and return as an array
+            // Code omitted for brevity
+        }
+    }
+}
---
+++ Titanic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titanic
+{
+    public class Titanic
+    {
+        private readonly IPassengerDataProvider _passengerDataProvider;
+
+        public Titanic(IPassengerDataProvider passengerDataProvider)
+        {
+            _passengerDataProvider = passengerDataProvider;
+        }
+
+        public int NumberOfPassengers => _passengerDataProvider.GetAllPassengers().Length;
+
+        public int NumberOfSurvivors
+        {
+            get
+            {
+                var passengers = _passengerDataProvider.GetAllPassengers();
+                return passengers.Count(p => p.IsSurvivor);
+            }
+        }
+    }
+}
